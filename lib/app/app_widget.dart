28498a9fda6733ac8bf99e1c1a695bd890ed54ab
jacksonAbrao <jackson.comp1681@hotmail.com>
2022-07-10 21:07:38 -0300

    first commit

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:asuka/asuka.dart' as asuka;
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Modular.setObservers([asuka.asukaHeroController]);
+
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      builder: (_, __) => MaterialApp.router(
+        title: 'Cuida Pet',
+        debugShowCheckedModeBanner: false,
+        builder: asuka.builder,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        routeInformationParser: Modular.routeInformationParser,
+        routerDelegate: Modular.routerDelegate,
+      ),
+    );
+  }
+}
