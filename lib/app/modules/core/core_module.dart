4eec8e33184de02e417981b3030ab8ee7a498582
jackson.comp1681@hotmail.com <jackson.comp1681@hotmail.com>
2022-07-13 18:06:18 -0300

    alt

--- lib/app/modules/core/core_module.dart
+++ lib/app/modules/core/core_module.dart
@@ -1,3 +1,7 @@
+import 'package:cuida_pet/app/core/local_storage/local_storage.dart';
+import 'package:cuida_pet/app/core/local_storage/shared_preferences/shared_preferences_local_storage_impl.dart';
+import 'package:cuida_pet/app/core/loggger/app_logger.dart';
+import 'package:cuida_pet/app/core/loggger/logger_app_logger_impl.dart';
 import 'package:cuida_pet/app/core/rest_client/dio/dio_rest_client.dart';
 import 'package:cuida_pet/app/core/rest_client/rest_client.dart';
 import 'package:cuida_pet/app/modules/core/auth/auth_store.dart';
@@ -8,5 +12,8 @@ class CoreModule extends Module {
   final List<Bind> binds = [
     Bind.lazySingleton((i) => AuthStore(), export: true),
     Bind.lazySingleton<RestClient>((i) => DioRestClient(), export: true),
+    Bind.lazySingleton<AppLogger>((i) => LoggerAppLoggerImpl(), export: true),
+    Bind.lazySingleton<LocalStorage>((i) => SharedPreferencesLocalStorageImpl(),
+        export: true),
   ];
 }
